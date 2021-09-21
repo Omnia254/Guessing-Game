9b6da266443b869364e81932ff68ce12a68f77b6
Omnia254 <76522001+Omnia254@users.noreply.github.com>
2021-09-20 21:34:59 -0700

    Update Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,16 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
+#import random 
 import random
 number = random.randint(1, 10)
-
+#get Name of player
 player_of_name = input("Hello, What's your name?")
 number_of_guess = 0
 print('okay! '+ player_of_name+ ' I am Guessing a number between 1 and 10:')
-
+# condition of guesses
 while number_of_guess < 5:
     guess = int(input())
     number_of_guess += 1
@@ -20,26 +15,8 @@ while number_of_guess < 5:
         print('Your guess is too high')
     if guess == number:
         break
+#guesses number 
 if guess == number:
     print('You guessed the number in ' + str(number_of_guess) + ' tries!')
 else:
     print('You did not guess the number, The number was ' + str(number))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
